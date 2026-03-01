af4cd009e7752e302d4d6fefd7f5e1698fe2b1ad
Allan Pires <doislan@gmail.com>
2026-03-01 12:42:13 -0800

    Fix github repo link

--- midi_to_macro/version.py
+++ midi_to_macro/version.py
@@ -3,7 +3,7 @@
 __version__ = "1.0.0"
 
 # GitHub repo for update checks: "owner/repo"
-GITHUB_REPO = "doisl/midi-to-macro-py"
+GITHUB_REPO = "allan-pires/where-songs-meet"
 
 GITHUB_RELEASES_API = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
 GITHUB_RELEASES_PAGE = f"https://github.com/{GITHUB_REPO}/releases"
